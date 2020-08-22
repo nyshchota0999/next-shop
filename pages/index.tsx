fccb63d1283aedbc308b397791c958e1ab8a892b
Igor <mr.nishchot@gmail.com>
2020-08-22 13:49:29 +0300

    add styled components library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.h1`
+  font-size: 2em;
+  text-align: left;
+  padding: 16px;
+`;
 
 const Index = () => {
-  return <div>Index page</div>;
+  return (
+    <div>
+      <StyledHeader>Index page text</StyledHeader>
+    </div>
+  );
 };
 
 export default Index;
